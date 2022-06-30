d6214854d873aa8b4fc609451a7ad168f61b57b6
Lucas Freitas <lucas@xsys.com.br>
2022-06-30 12:20:08 -0300

    Ajuste referente ao usuário com matrícula ativa e inativa.

--- boletoDownloader.js
+++ boletoDownloader.js
@@ -12,17 +12,17 @@ const accessPDFDownloadPage = async ()  => {
     const userNameBar = await driver.findElement(By.xpath('//*[@id="usuario-input"]'));
     const userPasswordBar = await driver.findElement(By.xpath('//*[@id="senha-input"]'));
     await userNameBar.sendKeys('YOUR_STUDENT_ID');
-    await sleep(3000);
+    await sleep(1000);
     await userPasswordBar.sendKeys('YOUR_PASSWORD')
     await userPasswordBar.sendKeys(Key.ENTER);
-    await sleep(3000);
+    await sleep(1000);
     const financialOption = await driver.findElement(By.xpath('//*[@id="navbar-menu"]/li[2]/a'))
     await financialOption.sendKeys(Key.ENTER);
     await sleep(1000);
-    const extractOption = await driver.findElement(By.xpath('//*[@id="navbar-menu"]/li[2]/ul/li[1]/a'));
+    const extractOption = await driver.findElement(By.xpath('//a[text()="Extrato"]'));
     await extractOption.sendKeys(Key.ENTER);
 
-    const actionDownload = await driver.findElement(By.xpath('//*[@id="6856566"]/td[8]/a'));
+    const actionDownload = await driver.findElement(By.xpath('//a[contains(text(), "Baixar Boleto")]'));
     await actionDownload.click();
     const downloadButton = await driver.findElement(By.xpath('//*[@id="btn-dwnBoleto"]'));
     await sleep(2000);
